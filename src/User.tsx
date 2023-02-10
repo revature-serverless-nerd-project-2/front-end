dbce7cd25ede1a84242a43c0ce9199c7a59e4e4f
Jose Paolo Sese <jpaolo.sese@gmail.com>
2023-02-10 13:41:43 -0800

    Worked out git remote repository issues on local machine

---
+++ src/User.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const User = () => {
+
+    const [user, setUser] = useState()
+
+  return (
+    <div>User</div>
+  )
+}
+
+export default User
