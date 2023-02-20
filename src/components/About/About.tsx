452ffeb5050aaf340657add77fca24d1644c29e9
mcmillenchrisj <mcmillenchrisj@gmail.com>
2023-02-20 01:32:01 -0500

    final css stylings -cm

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,11 +3,11 @@ import './About.css';
 
 export function AboutUs() {
     return(
-        <><div className="about-section">
+        <><main><div className="about-section">
             <h1>About Us</h1>
             <p>We are a small business who strive to help match our customers with their tech needs!</p>
-            <p>We are a new </p>
-        </div><h2>Our Team</h2><div className="row">
+            <p>We are a new to online and we want to try to reach as many people as possible and build great relationships with everyone with our products.</p>
+        </div><h2 className="header">Our Team</h2><div className="row">
                 <div className="column">
                     <div className="card">
                         <img src="/w3images/team1.jpg" alt="Jane"/>
@@ -46,5 +46,5 @@ export function AboutUs() {
                             </div>
                         </div>
                 </div>
-            </div></>
+            </div></main></>
 )}
